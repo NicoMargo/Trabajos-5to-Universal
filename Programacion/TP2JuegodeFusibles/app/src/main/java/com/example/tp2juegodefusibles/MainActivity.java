6f6c1f8e3c9f5b766166f440fc48df4489831383
Android Studio <devandroid@ort>
2019-04-15 09:19:45 -0300

    comenzando recaptcha con un problema en un implement en el archivo build.gradle Module:app

--- Programacion/TP2JuegodeFusibles/app/src/main/java/com/example/tp2juegodefusibles/MainActivity.java
+++ Programacion/TP2JuegodeFusibles/app/src/main/java/com/example/tp2juegodefusibles/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(btnStart_Click);
     }
+
+    private View.OnClickListener btnStart_Click = new View.OnClickListener() {
+        public void onClick(View v) {
+            EditText edtUser = (EditText) findViewById(R.id.edtUser);
+            String sUsername = edtUser.getText().toString();
+            if (sUsername.matches("")){
+                Toast.makeText(getApplicationContext(), "Ingrese un Nombre de usuario", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    };
 }
