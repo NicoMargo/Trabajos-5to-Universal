7f6d456340b02c3dd505528b92f1dd905462cb47
Android Studio <devandroid@ort>
2019-04-15 09:18:36 -0300

    xd

--- Programacion/TP2JuegodeFusibles/app/src/main/java/com/example/tp2juegodefusibles/Boton.java
+++ Programacion/TP2JuegodeFusibles/app/src/main/java/com/example/tp2juegodefusibles/Boton.java
@@ -3,19 +3,25 @@ package com.example.tp2juegodefusibles;
 import android.widget.Button;
 
 public class Boton{
-    private Button Boton;
+    private Button btnButton;
     private boolean bActivo;
     public Boton (Button unBoton, boolean Activo){
-        this.Boton = unBoton;
+        this.btnButton = unBoton;
         this.bActivo = Activo;
     }
-    public Button getBoton(){
-        return this.Boton;
+    public Button getButton(){
+        return this.btnButton;
     }
     public boolean getActivo(){
         return this.bActivo;
     }
     public void setActivo(){
         this.bActivo = !this.bActivo;
+        if(this.bActivo){
+            this.btnButton.setBackgroundResource(R.drawable.fusible_rojo);
+        }
+        else{
+            this.btnButton.setBackgroundResource(R.drawable.fusible_rojo);
+        }
     }
 }
--- Programacion/TP2JuegodeFusibles/app/src/main/java/com/example/tp2juegodefusibles/GameActivity.java
+++ Programacion/TP2JuegodeFusibles/app/src/main/java/com/example/tp2juegodefusibles/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.tp2juegodefusibles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.ConsoleMessage;
 import android.widget.Button;
 
@@ -11,20 +12,45 @@ import java.util.logging.ConsoleHandler;
 
 public class GameActivity extends AppCompatActivity {
 
-    private Boton Botones[][];
+    private Boton Buttons[][];
+
+    private final int iCantElementosX = 3;
+    private final int iCantElementosY = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        final int iCantElementosX = 3;
-        final int iCantElementosY = 3;
-        Botones = new Boton[iCantElementosX][iCantElementosY];
+        Buttons = new Boton[iCantElementosX][iCantElementosY];
         Log.d("xd","xd");
 
     }
 
     private void getReferences(){
-
-
+        Buttons[0][0] = new Boton((Button)findViewById(R.id.btn00),false);
+        Buttons[0][1] = new Boton((Button)findViewById(R.id.btn01),false);
+        Buttons[0][2] = new Boton((Button)findViewById(R.id.btn02),false);
+        Buttons[1][0] = new Boton((Button)findViewById(R.id.btn03),false);
+        Buttons[1][1] = new Boton((Button)findViewById(R.id.btn04),false);
+        Buttons[1][2] = new Boton((Button)findViewById(R.id.btn05),false);
+        Buttons[2][0] = new Boton((Button)findViewById(R.id.btn06),false);
+        Buttons[2][1] = new Boton((Button)findViewById(R.id.btn07),false);
+        Buttons[2][2] = new Boton((Button)findViewById(R.id.btn08),false);
     }
+
+    private void SetearListeners() {
+        for(int i = 0; i<iCantElementosX;i++){
+            for(int j = 0; j<iCantElementosY;j++){
+               // Buttons[i][j].getButton().setOnClickListener(Buttons[i][j].getButton());
+            }
+        }
+    }
+
+
+    private View.OnClickListener btnTest_Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };//Fin de la funcion onClickListener
+
 }
