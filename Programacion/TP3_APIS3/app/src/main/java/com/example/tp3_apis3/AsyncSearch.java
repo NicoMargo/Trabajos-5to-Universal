214be387c08c6eaf8463c15c6be1cd9d37b8c919
Android Studio <devandroid@ort>
2019-07-15 19:15:05 -0300

    avanzando con tp de apis

---
+++ Programacion/TP3_APIS3/app/src/main/java/com/example/tp3_apis3/AsyncSearch.java
@@ -0,0 +1,101 @@
+package com.example.tp3_apis3;
+import android.os.AsyncTask;
+import android.util.JsonReader;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AsyncSearch extends AsyncTask<Void,Void,Void> {
+    ArrayAdapter  _myAdapter;
+    ListView _lvList;
+    String _action;
+
+    public AsyncSearch(String action, String param, String value){
+        super();
+        _action = action +"/?"+param+"="+value;
+    }
+
+    public AsyncSearch(String action, float x, float y ){
+        super();
+        _action = action +"/reverseGeocoderLugares/?x="+x+"y="+y;
+    }
+
+    public void SetAdapterAndArray(ArrayAdapter  myAdapter, ListView myListView){
+        _myAdapter = myAdapter;
+        _lvList = myListView;
+    }
+    @Override
+    protected Void doInBackground(Void ... voids){
+        try{
+            URL myRoute = new URL("http://epok.buenosaires.gob.ar/"+ _action);
+            HttpURLConnection myConection =(HttpURLConnection) myRoute.openConnection();
+            Log.d("AccesoApi","Primer Conectando...");
+            Log.d("AccesoAPI", "" + myConection.getResponseCode());
+            if(myConection.getResponseCode() ==200){
+                Log.d("AccesoApi","OK!");
+                InputStream bodyResponse = myConection.getInputStream();
+                InputStreamReader responseReader = new InputStreamReader(bodyResponse,"UTF-8");
+                ProcessJSON(responseReader);
+            }
+            else{
+                Log.d("AccesoApi","Alto Fail We xd!");
+            }
+            myConection.disconnect();
+        }
+        catch(MalformedURLException error){
+            Log.d("AccesoAPI","Url incorrecta");
+        } catch (IOException error) {
+            Log.d("AccesoAPI","IO");
+        }
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Void aVoid){
+        super.onPostExecute(aVoid);
+        _lvList.setAdapter(_myAdapter);
+    }
+
+
+    public void ProcessJSON(InputStreamReader ReadStream){//Read Past Tense :V
+        JsonReader myJsonReader= new JsonReader(ReadStream);
+        try{
+            myJsonReader.beginObject();
+            while(myJsonReader.hasNext()){
+                Log.d("API","Kamisama puede ser muy cruel");
+                String objName = myJsonReader.nextName();
+                if(objName.equals("cantidad_de_categorias")){
+                    int quantCategories = myJsonReader.nextInt();
+                }
+                else{
+                    myJsonReader.beginArray();
+                    while(myJsonReader.hasNext()){
+                        myJsonReader.beginObject();
+                        while(myJsonReader.hasNext()){
+                            objName = myJsonReader.nextName();
+                            if(objName.equals("nombre")){
+                                String CategoryName = myJsonReader.nextString();
+                                Log.d("API","Energia recuperada "+ CategoryName);
+                                byCatFragment.listCat.add(CategoryName);
+                            } else{
+                                myJsonReader.skipValue();
+                            }
+                        }
+                        myJsonReader.endObject();
+                    }
+                    myJsonReader.endArray();
+                }
+            }
+        }//Fin del try
+        catch(Exception e){
+
+        }
+
+    }
+}
