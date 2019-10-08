d66f84945643e0025cd27d489b6ddbb900e1f169
Android Studio <devandroid@ort>
2019-10-08 12:09:15 -0300

    cambios minimos 2 del tp 10

--- EFSI/TP10/app/res/JS/Index.js
+++ EFSI/TP10/app/res/JS/Index.js
@@ -6,7 +6,22 @@ $(document).ready(function () {
 		success: function (Producto) {	
 					
 			for(var i = 0; i< Producto.length;i++){				
-				$("#cardGroup").append('<div class="card mb-3 mr-3 col-md-3"><div style="height: 250px;"><img class="card-img-top" src="../res/img/'+Producto[i].foto+'.jpg" alt="Card image cap"></div><div class="card-body"><div class="d-flex"><h5 class="card-title mr-auto">'+Producto[i].nombre+'</h5><div class="text-success font-weight-bold">$'+Producto[i].precio+'</div></div><div class="d-flex justify-content-between"><button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Modificar</button></div></div></div>');	
+				$("#cardGroup").append(
+				'<div class="card mb-3 mr-3 col-md-3"><div style="height: 250px;">'+
+					'<img class="card-img-top" src="../res/img/'+Producto[i].foto+'.jpg" alt="Card image cap">'+
+				'</div>'+
+				'<div class="card-body">'+
+					'<div class="d-flex">'+
+					'<h5 class="card-title mr-auto">'+Producto[i].nombre+'</h5>'+
+						'<div class="text-success font-weight-bold">$'+Producto[i].precio+'</div>'+
+					'</div>'+
+						'<div class="d-flex justify-content-between">'+
+							'<modificar><button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Modificar</button></modificar>'+
+							'<borrar><button type="button" class="btn btn-danger ml-3" data-toggle="modal" data-target=".bd-example-modal-lg">Borrar</button></borrar>'+
+						'</div>'+
+					'</div>'+
+				'</div>'
+				);	
 			}			
 		},
 		error: function () {
