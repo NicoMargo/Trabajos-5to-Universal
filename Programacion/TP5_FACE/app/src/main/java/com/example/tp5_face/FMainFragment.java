0228209eb4ad5aa596233cea8750bd750cb103d6
Android Studio <devandroid@ort>
2019-10-23 09:05:45 -0300

    continaundo el tp 5 Face recognition lol xd

--- Programacion/TP5_FACE/app/src/main/java/com/example/tp5_face/FMainFragment.java
+++ Programacion/TP5_FACE/app/src/main/java/com/example/tp5_face/FMainFragment.java
@@ -5,16 +5,19 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
@@ -25,6 +28,7 @@ public class FMainFragment extends Fragment {
     View rootView;
     Button btnCamera;
     Button btnStorage;
+    ImageView imgResult;
 
     private final int CODE_CAMERA_ACTIVITY = 1;
     private final int CODE_STORAGE_ACTIVITY = 2;
@@ -54,29 +58,33 @@ public class FMainFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        if(requestCode == CODE_PERMISSION_REQUEST);
-        boolean boolPermissionsGranted = true;
-        int i = 0;
-        while(i < permissions.length && boolPermissionsGranted){
-            boolPermissionsGranted &= (grantResults[i] != PackageManager.PERMISSION_GRANTED);
-            i++;
-        }
-        if(boolPermissionsGranted){
-            btnCamera.setEnabled(true);
-        }
-    }
-
     private void getReferences(){
         btnCamera = rootView.findViewById(R.id.btnCamera);
         btnStorage = rootView.findViewById(R.id.btnStorage);
+        imgResult = rootView.findViewById(R.id.imgResult);
     }
 
     private void setListeners(){
         btnCamera.setOnClickListener(btnCamera_Click);
         btnStorage.setOnClickListener(btnStorage_Click);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(requestCode == CODE_PERMISSION_REQUEST) {
+            boolean boolPermissionsGranted = true;
+            int i = 0;
+            while (i < permissions.length && boolPermissionsGranted) {
+                boolPermissionsGranted &= (grantResults[i] != PackageManager.PERMISSION_GRANTED);
+                i++;
+            }
+            if (boolPermissionsGranted) {
+                btnCamera.setEnabled(true);
+            }
+        }
+        Log.d("hola","xd");
+    }
+
 
     public View.OnClickListener btnCamera_Click = new View.OnClickListener(){
         public void onClick(View v){
@@ -99,5 +107,15 @@ public class FMainFragment extends Fragment {
         Intent intentStorage = new   Intent(Intent.ACTION_GET_CONTENT);
         intentStorage.setType("image/*");
         startActivityForResult(Intent.createChooser(intentStorage,"Seleccione foto"),CODE_STORAGE_ACTIVITY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent intent ){
+        super.onActivityResult( requestCode, resultCode, intent);
+        if(requestCode == CODE_CAMERA_ACTIVITY){
+            Bitmap imageResult = (Bitmap) intent.getExtras().get("data");
+            imgResult.setImageBitmap(imageResult);
+        }
+
     }
 }
